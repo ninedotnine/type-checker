7608a7ce467a9b79864a968001ee8362ff94e253
dan soucy <dev@danso.ca>
2020-03-02 00:28:38 -0500

    add sum type

--- typechecker.py
+++ typechecker.py
@@ -67,13 +67,37 @@ class Arrow(BaseType):
         return f"({self.domain} -> {self.rang})"
 
 
+class Sum(BaseType):
+    tag = "+"
+
+    def __init__(self, left, right):
+        if not judgement_type(left):
+            raise TypeError("'left' is not a valid type")
+        if not judgement_type(right):
+            raise TypeError("'right' is not a valid type")
+
+        self.left = left
+        self.right = right
+
+    def __eq__(self, other):
+        try:
+            return (other.tag == "+"
+                    and self.left == other.left
+                    and self.right == other.right)
+        except AttributeError:
+            return False
+
+    def __repr__(self):
+        return f"({self.left} + {self.right})"
+
+
 def judgement_type(thing):
     """returns true if its argument is a valid type"""
     try:
         if thing.tag == "->":
             return judgement_type(thing.domain) and judgement_type(thing.rang)
 
-        if thing.tag == "&":
+        if thing.tag == "&" or thing.tag == "+":
             return judgement_type(thing.left) and judgement_type(thing.right)
 
         return thing.tag.isalpha()
@@ -164,6 +188,36 @@ class SplitPair():
         self.y_type = y_type
         self.body = body
 
+    def __repr__(self):
+        return f"{self.body} where {self.x_name}:{self.x_type} and {self.y_name}:{self.y_type}"
+
+
+class Either():
+    tag = "|"
+
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    def __repr__(self):
+        return f"{self.first} | {self.second}"
+
+class SplitEither():
+    # pattern matching on Either
+    tag = "||"
+
+    def __init__(self, e, x_name, x_type, y_name, y_type, body):
+        if not judgement_type(x_type):
+            raise TypeError("'x_type' is not a valid type")
+        if not judgement_type(y_type):
+            raise TypeError("'y_type' is not a valid type")
+
+        self.either = e
+        self.x_name = x_name
+        self.x_type = x_type
+        self.y_name = y_name
+        self.y_type = y_type
+        self.body = body
 
     def __repr__(self):
         return f"{self.body} where {self.x_name}:{self.x_type} and {self.y_name}:{self.y_type}"
@@ -212,6 +266,11 @@ def judgement_check(context, m, a):
         return (judgement_check(context, m.pair, Pair(m.x_type, m.y_type))
                 and judgement_check(new_context, m.body, a))
 
+    if m.tag == "||":
+        new_context = Context({m.x_name: m.x_type, m.y_name: m.y_type}, context)
+        return (judgement_check(context, m.either, Sum(m.x_type, m.y_type))
+                and judgement_check(new_context, m.body, a))
+
     if m.tag == "\\":
         if a.tag != "->":
             print("function should have type ->")
@@ -241,6 +300,11 @@ def demo_types():
     assert( judgement_type(p) )
     assert( p == Pair(Foo, Bar) )
 
+    s = Sum(x,y)
+    print(f"s is {s}")
+    assert( judgement_type(s) )
+    assert( s == Sum(Foo, Bar) )
+
     f = Arrow(x,p)
     print(f"f is {f}")
     assert( judgement_type(f) )
@@ -267,6 +331,7 @@ def demo_contexts():
     print(f"v2 is {v2}")
     c2 = Context({"val": Bar}, c)
     print(f"c2 is {c2}")
+    assert( judgement_ctx(c2) )
     assert( judgement_check(c2, v2, Bar) )
 
     v3 = MakePair(v, v2)
@@ -275,6 +340,7 @@ def demo_contexts():
 
     c3 = Context({"pairofvars": Pair(Foo, Bar)}, e)
     print(f"c3 is {c3}")
+    assert( judgement_ctx(c3) )
 
     v4 = SplitPair(V("pairofvars"), "var", Foo, "val", Bar, V("var"))
     print(f"v4 is {v4}")
@@ -286,9 +352,18 @@ def demo_contexts():
 
     c4 = Context({"f": Arrow(Foo, Bar)}, c2)
     print(f"c4 is {c4}")
+    assert( judgement_ctx(c4) )
     v6 = Apply(V("f"), V("var"), Foo)
     print(f"v6 is {v6}")
     assert( judgement_check(c4, v6, Bar) )
+
+    c5 = Context({"eitherfooorbar": Sum(Foo, Bar)}, e)
+    print(f"c5 is {c5}")
+    assert( judgement_ctx(c5) )
+    v7 = SplitEither(V("eitherfooorbar"), "var", Foo, "val", Bar, V("var"))
+    print(f"v7 is {v7}")
+    assert( judgement_check(c5, v7, Foo) )
+
 
 
 def examples():
